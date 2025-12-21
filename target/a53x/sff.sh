fb1540e66ce59704860dc9746a3b9cbf074bb608
Majaahh <maja.blaszczak@proton.me>
2025-12-21 22:04:54 +0100

    target: a53x: init
    * The only changes to recovery.fstab are:
      - Remove unnecessary lines at the end of the file
      - Add a space for last comment
    
    Change-Id: I89351d77c55cd54ffb38381e1dbe03739f6e4f42
    Co-authored-by: Salvo Giangreco <giangrecosalvo9@gmail.com>
    Signed-off-by: Majaahh <maja.blaszczak@proton.me>

---
+++ target/a53x/sff.sh
@@ -0,0 +1,10 @@
+# Copyright (c) 2026 Majaahh
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+# SEC Floating Feature configuration file for Galaxy A53 5G (a53x)
+
+# Gallery
+SEC_FLOATING_FEATURE_GALLERY_CONFIG_IMAGE_TAGGER_VERSION=V701
+
+# Refresh Rate
+SEC_FLOATING_FEATURE_LCD_CONFIG_HFR_MODE=2
